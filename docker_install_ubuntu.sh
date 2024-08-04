d9ec12f2caaf8df86bb4a676f5a2203afb826d68
rnarainsamy <115066451+rnarainsamy@users.noreply.github.com>
2024-08-04 14:17:29 +0200

    Update docker_install_ubuntu.sh
    merci Eddie

--- docker_install_ubuntu.sh
+++ docker_install_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-sudo apt-get update
+sudo apt-get update && apt-get upgrade -y
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
